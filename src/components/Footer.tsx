8f4383b718a2532892e5f905c7997253a8ba9404
ochentso <ochentso@gmail.com>
2024-05-16 14:17:40 +0300

    fix contacts links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,11 +5,11 @@ export const Footer = () => (
   <footer className="flex justify-between py-4 px-7 md:px-9 md:py-6 mt-4 md:mt-6 border-t-2 border-warmGray">
     <div className="flex gap-1 text-mainPurple text-xs md:text-sm lg:text-base">
       <MailIcon />
-      <span>ochentso@gmail.com</span>
+      <a href="mailto:ochentso@gmail.com">ochentso@gmail.com</a>
     </div>
     <div>
       <a
-        href=""
+        href="https://github.com/ochentso"
         target="_blank"
         className="flex gap-1 text-mainPurple text-xs md:text-sm lg:text-base"
       >
